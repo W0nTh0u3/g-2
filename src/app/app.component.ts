94db0187ab76915f76d4bbb53c7bcd07f8081eae
Ryan Verzo <ryanemmanuelvrz345@gmail.com>
2024-09-06 12:19:24 +0800

    website 50%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactUsComponent } from "./components/contact-us/contact-us.component";
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, ContactUsComponent, FooterComponent, MainComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'g-2';
+	title = 'g-2';
 }
