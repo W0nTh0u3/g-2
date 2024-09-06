d9655c824013e359afe0bc4af8502b1a78c62186
Ryan Verzo <ryanemmanuelvrz345@gmail.com>
2024-09-06 14:35:52 +0800

    ui changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ContactUsComponent } from "./components/contact-us/contact-us.component";
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, ContactUsComponent, FooterComponent, MainComponent],
+	imports: [RouterOutlet, ContactUsComponent, FooterComponent, MainComponent, HeaderComponent],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss'
 })
